4901f7d8810be1556c1ccac2485c695303675e92
hexWars <tech@sehnsucht.top>
2023-04-02 18:51:19 +0800

    :sparkles: rust学习

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,12 +10,20 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples (num: i32) -> i32 {
+    if num <= 40 {
+        num * 2
+    } else {
+        num
+    }
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
